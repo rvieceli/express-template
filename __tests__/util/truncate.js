4371b6379211f86c59f22a5753a2337916d96584
Rafael Ricardo Vieceli <rafaelricardo3@gmail.com>
2020-01-07 14:45:29 +0100

    test: cheking problem with sqlite

--- __tests__/util/truncate.js
+++ __tests__/util/truncate.js
@@ -1,14 +1,14 @@
-// import database from '../../src/database';
+import database from '../../src/database';
 
 // TODO: ver proque da erro de IO
 
 export default function truncate() {
-  // return Promise.all(
-  //   Object.keys(database.connection.models).map(model => {
-  //     return database.connection.models[model].destroy({
-  //       truncate: true,
-  //       force: true,
-  //     });
-  //   })
-  // );
+  return Promise.all(
+    Object.keys(database.connection.models).map(model => {
+      return database.connection.models[model].destroy({
+        truncate: true,
+        force: true,
+      });
+    })
+  );
 }
